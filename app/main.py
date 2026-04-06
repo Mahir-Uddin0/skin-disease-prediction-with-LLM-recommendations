61effc5e2652141dc4986f0108ce483639e86e96
Mahir-Uddin0 <mahir.uddin.0@gmail.com>
2026-04-06 22:09:54 +0600

    created FastAPI entrypoint

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.api.v1.router import api_router
+
+app = FastAPI(
+    title="Skin Disease Detection API",
+    version="1.0.0"
+)
+
+app.include_router(api_router, prefix="/api/v1")
+
+
+@app.get("/")
+def root():
+    return {"message": "API is running"}
