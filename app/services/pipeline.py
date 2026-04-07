07df9e5148024e0f2fc7381ecdb87b1640b4df87
Mahir-Uddin0 <mahir.uddin.0@gmail.com>
2026-04-07 23:49:37 +0600

    modified analyze_skin function to return llm recommendation

--- app/services/pipeline.py
+++ app/services/pipeline.py
@@ -1,10 +1,13 @@
-from app.services.inference_service import predict_disease
-
+from app.services.llm_service import generate_recommendation, parse_llm_output
 
 def analyze_skin(image_bytes: bytes):
     disease, confidence = predict_disease(image_bytes)
 
+    llm_raw = generate_recommendation(disease, confidence)
+    llm_data = parse_llm_output(llm_raw)
+
     return {
         "disease": disease,
-        "confidence": confidence
+        "confidence": confidence,
+        **llm_data
     }
