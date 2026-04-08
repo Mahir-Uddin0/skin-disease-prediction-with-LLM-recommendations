cb207ce168c43718e976fb7a8301fe166f244bf8
Mahir-Uddin0 <mahir.uddin.0@gmail.com>
2026-04-08 12:17:02 +0600

    completed disease prediction function

---
+++ app/ml/inference/predict.py
@@ -0,0 +1,24 @@
+import torch
+from PIL import Image
+import io
+
+from app.ml.models.model_loader import model, device
+from app.ml.preprocessing.transform import transform
+from app.utils.constants import CLASS_NAMES
+
+
+def predict_image(image_bytes: bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    image = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        outputs = model(image)
+        probs = torch.nn.functional.softmax(outputs, dim=1)
+
+        confidence, pred_class = torch.max(probs, dim=1)
+
+    disease = CLASS_NAMES[(pred_class-1).item()]
+    confidence = confidence.item()
+
+    return disease, confidence
