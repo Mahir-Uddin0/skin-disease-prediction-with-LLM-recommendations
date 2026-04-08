0bfa939c823d4219584556cf8c439d1a1c7f4f51
Mahir-Uddin0 <mahir.uddin.0@gmail.com>
2026-04-08 12:21:55 +0600

    defined disease classes

---
+++ app/utils/constants.py
@@ -0,0 +1,12 @@
+CLASS_NAMES = [
+    "Eczema",
+    "Melanoma",
+    "Atopic Dermatitis",
+    "Basal Cell Carcinoma (BCC) ",
+    "Melanocytic Nevi (NV)",
+    "Benign Keratosis-like Lesions (BKL)",
+    "Psoriasis pictures Lichen Planus and related diseases",
+    "Seborrheic Keratoses and other Benign Tumors",
+    "Tinea Ringworm Candidiasis and other Fungal Infections",
+    "Warts Molluscum and other Viral Infections"
+]
